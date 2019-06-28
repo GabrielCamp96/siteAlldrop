4ae372f2ea2bc3bd16849b7c62b63ab43b4a2dba
Gabriel Campanário <gabriel.camp96@hotmail.com>
2019-06-28 17:48:48 -0300

    bugfix e cards

--- gulpTasks/deps.js
+++ gulpTasks/deps.js
@@ -18,7 +18,9 @@ gulp.task('deps.js', () => {
 gulp.task('deps.css', () => {
     return gulp.src([
         'node_modules/bootstrap/dist/css/bootstrap.min.css',
-        'node_modules/@fortawesome/fontawesome-free/css/all.css'
+        'node_modules/@fortawesome/fontawesome-free/css/all.css',
+        'node_modules/animate.css/source/_base.css',
+        'node_modules/animate.css/source/fading_entrances/fadeInDown.css'
     ])
         .pipe(uglifycss())
         .pipe(concat('deps.min.css'))
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,37 @@
 jQuery(function(){
     $('.carousel').carousel({
-    interval: 2000
+    interval: 1000
+    })
+
+    $('body').scrollspy({
+        target: '#mainNav'
+    });
+
+    $('a.js-scroll-trigger').click(e => {
+        e.preventDefault()
+        const link = e.target.getAttribute('href').substr(1)
+        // console.log(link, typeof link)
+        document.querySelector(`section[id=${link}]`)
+            .scrollIntoView({ block: 'start', behavior: 'smooth' })
+    });
+
+    $(function () {
+        $(document).scroll(function () {
+            var $nav = $(".navbar")
+            var $topbar = $('.top-bar')
+            if ($(this).scrollTop() > 0) $topbar.hide()
+            else $topbar.show()
+            $nav.toggleClass('animated fadeInDown fixed-top', $(this).scrollTop() > $nav.height())
+        });
+    });
+
+    $('.single_service_part').hover(e => {
+        const target = $(e.target).find('hr')
+        const $hr = target
+        $hr.css('border-top', '2px solid #fff')
+    }, e => {
+        const target = $(e.target).find('hr')
+        const $hr = target
+        $hr.css('border-top', '2px solid #fe5c24')        
     })
 })
